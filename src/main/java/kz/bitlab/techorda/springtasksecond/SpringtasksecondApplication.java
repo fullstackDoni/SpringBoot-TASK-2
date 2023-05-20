460fe3f1ee5e98a6b7e0a7917d51965d47a748d5
Doни <daniyar.doskulov@narxoz.kz>
2023-05-20 23:16:41 +0600

    first commit

---
+++ src/main/java/kz/bitlab/techorda/springtasksecond/SpringtasksecondApplication.java
@@ -0,0 +1,13 @@
+package kz.bitlab.techorda.springtasksecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtasksecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtasksecondApplication.class, args);
+	}
+
+}
